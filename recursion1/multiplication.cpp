7cc1923115802cfb287ee80b67895f953303ba43
Pratham Bansal <bansalpratham.bt21cse@pec.edu.in>
2022-08-28 11:39:35 +0530

    Create multiplication.cpp

---
+++ recursion1/multiplication.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int multiplyNumbers(int m, int n) {
+    if(n==1){
+        return m;
+    }
+    else{
+        return m+multiplyNumbers(m,n-1);
+    }
+
+}
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    cout << multiplyNumbers(m, n) << endl;
+}
